541aadc3a82b7f15eab3078aff46e67ed2fcbc98
Jakubem <jakubmx@gmail.com>
2019-07-02 10:25:21 +0200

    Implement basic calculation logic
    Generic functions are located in utils file

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,96 @@
-console.log(':)');
+const parseOutput = require('./utils.js').parseOutput;
+const getResult = require('./utils.js').getResult;
+const equationIsValid = require('./utils.js').equationIsValid;
+
+const acBtn = document.querySelector('.button__ac');
+
+const output = document.querySelector('.screen__output');
+const equation = document.querySelector('.screen__equation');
+const numbers = document.querySelector('.buttons__numbers');
+const operations = document.querySelector('.buttons__operations');
+
+const operationsRegex = /\+|\-|\x|\÷/g;
+
+let currentOperation = '';
+let calculatedFinish = false;
+
+/**
+ * reset all previous states
+ */
+const resetState = () => {
+  currentOperation = '';
+  equation.value = '';
+  output.innerHTML = '0';
+  calculatedFinish = false;
+}
+
+resetState();
+
+acBtn.addEventListener('click', resetState);
+
+operations.addEventListener('click', (e) => {
+
+  if (calculatedFinish) {
+    resetState();
+    return;
+  } else if (equation.value !== '') {
+    inputOperator(e);
+  }
+})
+
+numbers.addEventListener('click', (e) => {
+
+  if (calculatedFinish) {
+    resetState();
+    return;
+  }
+
+  const key = e.target.dataset.value || '';
+
+  if (key === '.') {
+    inputDecimal(key);
+  } else {
+    inputNumber(key);
+  }
+});
+
+const inputOperator = (e) => {
+  const currentVal = equation.value;
+  const displayOperation = e.target.innerHTML;
+  const operation = e.target.dataset.operation;
+
+  if (operation === 'equals' && equationIsValid(currentVal, operationsRegex)) {
+    calculate();
+    equation.value += '=';
+    calculatedFinish = true;
+    return;
+  } else {
+    if (currentVal.match(operationsRegex)) {
+      calculate();
+      return;
+    } else {
+      currentOperation = operation;
+      equation.value += displayOperation;
+    }
+  }
+
+}
+
+const inputNumber = (key) => {
+  equation.value += key;
+}
+
+const inputDecimal = (key) => {
+  const currentVal = equation.value;
+  if (!currentVal.includes('.') && currentVal !== '') {
+    equation.value = currentVal + key;
+  }
+}
+
+const calculate = () => {
+  const currentEquation = equation.value;
+  if (equationIsValid(currentEquation, operationsRegex)) {
+    const [a, b] = parseOutput(currentEquation, operationsRegex);
+    output.innerHTML = getResult(Number(a), Number(b), currentOperation);
+  }
+}
---
+++ src/js/utils.js
@@ -0,0 +1,54 @@
+/**
+ * convert string output to a and b values
+ * @param {str} eq - equation to be converted to array
+ * @param {regex} regex - chars for equation to be split on
+ */
+const parseOutput = (eq, regex) => {
+  const vals = eq.split(regex);
+  return vals;
+}
+
+/**
+ * return result of given equation
+ * @param {number} a - first character of the operation
+ * @param {number} b - second character of the operation
+ * @param {string} operation - operation to be performed
+ */
+const getResult = (a, b, operation) => {
+  switch (operation) {
+    case "add":
+      const add = a + b;
+      return Number.isInteger(add) ? add : add.toFixed(2);
+      break;
+    case "subtract":
+      const subtract = a - b;
+      return Number.isInteger(subtract) ? subtract : subtract.toFixed(2);
+      break;
+    case "divide":
+      const divide = a / b
+      return Number.isInteger(divide) ? divide : divide.toFixed(2);
+      break;
+    case "multiply":
+      const multiply = a * b;
+      return Number.isInteger(multiply) ? multiply : multiply.toFixed(2);
+      break;
+    default:
+      console.error(`${operation} is not a valid operation`);
+  }
+}
+
+/**
+ * check if the equation has 2 arguments
+ * @param {str} eq - equation to be passed
+ * @param {regex} regex - chars for equation to be split on
+ */
+const equationIsValid = (eq, regex) => {
+  const arr = eq.split(regex);
+  return arr.length === 2 && !arr.includes('');
+}
+
+module.exports = {
+  parseOutput,
+  getResult,
+  equationIsValid
+}
